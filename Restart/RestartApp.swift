37ad02e966e7b74b978f588efed21feaa29f9b57
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-03-30 00:09:23 -0400

    chore: initial commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ewerson on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
