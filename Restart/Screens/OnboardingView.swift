448005638f021f342eb176c9af3181bfa50aa023
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-01 00:51:29 -0400

    chore: header layout for onboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -10,24 +10,54 @@ import SwiftUI
 struct OnboardingView: View {
 	// MARK: - Property
 	@AppStorage("onboarding") var isOnboardingViewActive: Bool = true
-
+	
 	// MARK: - Body
-    var body: some View {
-		 VStack(spacing: 20){
-			 Text("OnBoarding")
-				 .font(.largeTitle)
-			 
-			 Button {
-				 isOnboardingViewActive = false
-			 } label: {
-				 Text("Iniciar")
-			 }
-		 }
-    }
+	var body: some View {
+		ZStack {
+			Color("ColorBlue")
+				.ignoresSafeArea(.all, edges: .all)
+			
+			VStack(spacing: 20){
+				// MARK: - Header
+				Spacer()
+				
+				VStack(spacing: 0){
+					Text("Compartilhar.")
+						.font(.system(size: 60))
+						.fontWeight(.heavy)
+						.foregroundColor(.white)
+					
+					Text("""
+							não é sobre o quanto doamos,
+							mas quanto amor colocamos ao doar
+						""")
+					.font(.title3)
+					.fontWeight(.light)
+					.foregroundColor(.white)
+					.multilineTextAlignment(.center)
+					.padding(.horizontal, 10)
+				}
+			
+				// MARK: - Center
+				ZStack{
+					
+				}
+				
+				Spacer()
+				
+				// MARK: - Footer
+				Button {
+					isOnboardingViewActive = false
+				} label: {
+					Text("Iniciar")
+				}
+			}
+		}
+	}
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
-    }
+	static var previews: some View {
+		OnboardingView()
+	}
 }
