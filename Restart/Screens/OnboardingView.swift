a7634a0047d90472e86b305af9df9bf91fc01571
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-02 20:33:31 -0400

    chore: center layout for onboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -40,7 +40,17 @@ struct OnboardingView: View {
 			
 				// MARK: - Center
 				ZStack{
+					Circle()
+						.stroke(.white.opacity(0.2), lineWidth: 40)
+						.frame(width: 260, height: 260, alignment: .center)
 					
+					Circle()
+						.stroke(.white.opacity(0.2), lineWidth: 80)
+						.frame(width: 260, height: 260, alignment: .center)
+					
+					Image("character-1")
+						.resizable()
+						.scaledToFit()
 				}
 				
 				Spacer()
