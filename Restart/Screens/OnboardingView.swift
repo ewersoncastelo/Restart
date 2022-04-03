518d4a7d866b3506b79a6d82ea943401587cc2e6
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-02 21:41:01 -0400

    chore: footer layout for onboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -28,16 +28,16 @@ struct OnboardingView: View {
 						.foregroundColor(.white)
 					
 					Text("""
-							não é sobre o quanto doamos,
-							mas quanto amor colocamos ao doar
-						""")
+não é sobre o quanto doamos,
+mas quanto amor colocamos ao doar
+""")
 					.font(.title3)
 					.fontWeight(.light)
 					.foregroundColor(.white)
 					.multilineTextAlignment(.center)
 					.padding(.horizontal, 10)
 				}
-			
+				
 				// MARK: - Center
 				ZStack{
 					Circle()
@@ -57,21 +57,48 @@ struct OnboardingView: View {
 				
 				// MARK: - Footer
 				ZStack{
-					 Capsule()
+					Capsule()
 						.fill(Color.white.opacity(0.2))
 					
 					Capsule()
 						.fill(Color.white.opacity(0.2))
 						.padding(8)
+					
+					Text("Iniciar")
+						.font(.system(.title3, design: .rounded))
+						.fontWeight(.bold)
+						.foregroundColor(.white)
+						.offset(x: 20)
+					
+					HStack{
+						Capsule()
+							.fill(Color("ColorRed"))
+							.frame(width: 80)
+						
+						Spacer()
+					}
+					
+					HStack {
+						ZStack{
+							Circle()
+								.fill(Color("ColorRed"))
+							Circle()
+								.fill(.black.opacity(0.15))
+								.padding(8)
+							Image(systemName: "chevron.right.2")
+								.font(.system(size: 24, weight: .bold))
+						}
+						.foregroundColor(.white)
+						.frame(width: 80, height: 80, alignment: .center)
+						.onTapGesture {
+							isOnboardingViewActive = false
+						}
+						
+						Spacer()
+					}
 				}
 				.frame(height: 80, alignment: .center)
 				.padding()
-				
-//				Button {
-//					isOnboardingViewActive = false
-//				} label: {
-//					Text("Iniciar")
-//				}
 			}
 		}
 	}
