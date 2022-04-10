3a8ad20e1148eb1258cf07a15ff7f29bb67649cd
Ewerson Castelo <ewerson.castelo@gmail.com>
2022-04-09 20:05:40 -0400

    chore: animating the CircleGroupView

--- Restart/Views/CircleGroupView.swift
+++ Restart/Views/CircleGroupView.swift
@@ -12,6 +12,8 @@ struct CircleGroupView: View {
 	@State var ShapeColor: Color
 	@State var ShapeOpacity: Double
 	
+	@State private var isAnimating: Bool = false
+	
 	var body: some View {
 		ZStack{
 			Circle()
@@ -21,6 +23,13 @@ struct CircleGroupView: View {
 			Circle()
 				.stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
 				.frame(width: 260, height: 260, alignment: .center)
+		}
+		.blur(radius: isAnimating ? 0: 10)
+		.opacity(isAnimating ? 1 : 0)
+		.scaleEffect(isAnimating ? 1: 0.5)
+		.animation(.easeOut(duration: 1), value: isAnimating)
+		.onAppear {
+			isAnimating = true
 		}
 	}
 }
